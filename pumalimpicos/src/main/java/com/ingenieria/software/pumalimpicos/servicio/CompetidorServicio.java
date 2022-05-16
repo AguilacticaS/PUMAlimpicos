ae39d20a5187cf9a3c22a78f1aaa4e429c2da318
armando122 <armandoramirezg@ciencias.unam.mx>
2022-05-16 12:47:48 -0500

    Competidor Servicio terminado

--- pumalimpicos/src/main/java/com/ingenieria/software/pumalimpicos/servicio/CompetidorServicio.java
+++ pumalimpicos/src/main/java/com/ingenieria/software/pumalimpicos/servicio/CompetidorServicio.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Clase CompetidorServicio
@@ -48,13 +49,48 @@ public class CompetidorServicio {
         .orElseThrow(() -> new IllegalStateException("No existe el competidor"));
     }
 
-    /***/
-    public void agregaCompetidor(){}
+    /**
+     * Método que agrega un competidor al repositorio.
+     * @param competidor El competidor a agregar.
+     **/
+    public void agregaCompetidor(Competidor competidor){
+        String nombre = competidor.getNombre();
+        Optional<Competidor> competidorOptional = competidorRepositorio.findCompetidorByNombre(nombre);
+        if (competidorOptional.isPresent()) {
+            throw new IllegalStateException("Ya existe el competidor con el nombre dado.");
+        }
+        competidorRepositorio.save(competidor);
+    }
 
-    /***/
-    public void eliminaCompetidor(){}
+    /**
+     * Método para eliminar un competidor del repositorio.
+     * @param competidorId Identificador del competidor.
+     **/
+    public void eliminaCompetidor(Long competidorId){
+            boolean exists = competidorRepositorio.existsById(competidorId);
+            if (!exists){
+                String advertencia = "Competidor con Id: " + competidorId + "no existe.";
+                throw new IllegalStateException(advertencia);
+            }
+            competidorRepositorio.deleteById(competidorId);
+    }
 
-    /***/
+    /**
+     * Método para actualizar los datos de un competidor.
+     * @param  competidorId Id del competidor.
+     * @param nombre Nombre del competidor.
+     * @param disciplina Disciplina del competidor.
+     **/
     @Transactional
-    public void actualizaCompetidor(){}
+    public void actualizaCompetidor(Long competidorId, String nombre, String disciplina){
+        String advertencia = "Competidor con Id: " + competidorId + "no existe.";
+        Competidor competidor = competidorRepositorio.findById(competidorId)
+                .orElseThrow(() -> new IllegalStateException(advertencia));
+
+        Boolean nom = nombre.equals(competidor.getNombre());
+        if (nombre != null && !nom) competidor.setNombre(nombre);
+
+        Boolean disci = disciplina.equals(competidor.getDisciplina());
+        if (disciplina != null && !disci) competidor.setDisciplina();
+    }
 }
