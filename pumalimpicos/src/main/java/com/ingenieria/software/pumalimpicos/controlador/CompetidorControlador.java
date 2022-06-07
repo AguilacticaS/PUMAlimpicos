a892dbcc9eef30714b277efe743a534fd4c5186c
armando122 <armandoramirezg@ciencias.unam.mx>
2022-06-07 18:44:37 -0500

    Vista agregada de tabla y CompetidorControlador

---
+++ pumalimpicos/src/main/java/com/ingenieria/software/pumalimpicos/controlador/CompetidorControlador.java
@@ -0,0 +1,35 @@
+package com.ingenieria.software.pumalimpicos.controlador;
+
+import com.ingenieria.software.pumalimpicos.servicio.CompetidorServicio;
+import com.ingenieria.software.pumalimpicos.servicio.DisciplinaServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/pos")
+public class CompetidorControlador {
+
+    /*Atributos*/
+    private final CompetidorServicio competidorServicio;
+    private final DisciplinaServicio disciplinaServicio;
+
+    /**
+     * Constructor
+     * @param competidorServicio
+     **/
+    @Autowired
+    CompetidorControlador(CompetidorServicio competidorServicio,
+                          DisciplinaServicio disciplinaServicio){
+        this.competidorServicio = competidorServicio;
+        this.disciplinaServicio = disciplinaServicio;
+    }
+
+    @GetMapping("/posiciones")
+    public String  posicionesCompetidor(Model model){
+        model.addAttribute("competidores",competidorServicio.getCompetidores());
+        return "competidor/posiciones";
+    }
+}
