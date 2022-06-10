f3d11cb2fff27b668e026eb02184ce0369a0f65b
IxchelVe <ix.ve.ca@ciencias.unam.mx>
2022-06-09 23:33:46 -0500

    nuevos competidores y cambios en botones de menús

--- pumalimpicos/src/main/java/com/ingenieria/software/pumalimpicos/CompetidorConfiguracion.java
+++ pumalimpicos/src/main/java/com/ingenieria/software/pumalimpicos/CompetidorConfiguracion.java
@@ -42,10 +42,106 @@ public class CompetidorConfiguracion {
                     passwordEncoder.encode("armando"),
                     "Tenis"
             );
+
+            Competidor competidor3 = new Competidor(
+                    "Alan",
+                    "Torres",
+                    "Ramírez",
+                    "alan@gmail.com",
+                    "alan",
+                    "Competidor",
+                    passwordEncoder.encode("alan"),
+                    "Tenis"
+            );
+
+            Competidor competidor4 = new Competidor(
+                    "Fernanda",
+                    "Herrera",
+                    "González",
+                    "fernanda@gmail.com",
+                    "fernanda",
+                    "Competidor",
+                    passwordEncoder.encode("fernanda"),
+                    "Boxeo"
+            );
+
+            Competidor competidor5 = new Competidor(
+                    "Javier",
+                    "Luna",
+                    "Velázquez",
+                    "javier@gmail.com",
+                    "javier",
+                    "Competidor",
+                    passwordEncoder.encode("javier"),
+                    "Atletismo"
+            );
+
+            Competidor competidor6 = new Competidor(
+                    "Clara",
+                    "Méndez",
+                    "Guerrero",
+                    "clara@gmail.com",
+                    "clara",
+                    "Competidor",
+                    passwordEncoder.encode("clara"),
+                    "Natación"
+            );
+
+            Competidor competidor7 = new Competidor(
+                    "María de Jesús",
+                    "Ortiz",
+                    "Vargas",
+                    "marichuy@gmail.com",
+                    "marichuy",
+                    "Competidor",
+                    passwordEncoder.encode("marichuy"),
+                    "Atletismo"
+            );
+
+            Competidor competidor8 = new Competidor(
+                    "Patricia",
+                    "Reyes",
+                    "Chávez",
+                    "paty@gmail.com",
+                    "paty",
+                    "Competidor",
+                    passwordEncoder.encode("paty"),
+                    "Natación"
+            );
+
+            Competidor competidor9 = new Competidor(
+                    "Sofia",
+                    "RLópez",
+                    "Cortez",
+                    "sofia@gmail.com",
+                    "sofia",
+                    "Competidor",
+                    passwordEncoder.encode("sofia"),
+                    "Futbol"
+            );
+
+            Competidor competidor10 = new Competidor(
+                    "José",
+                    "Castillo",
+                    "Guzmán",
+                    "josec@gmail.com",
+                    "josec",
+                    "Competidor",
+                    passwordEncoder.encode("josec"),
+                    "Halterofilia"
+            );
+
             repository.saveAll(
                     new ArrayList<Competidor>() {{
                         add(competidor1);
                         add(competidor2);
+                        add(competidor3);
+                        add(competidor4);
+                        add(competidor5);
+                        add(competidor6);
+                        add(competidor7);
+                        add(competidor8);
+                        add(competidor9);
                     }}
             );
         };
